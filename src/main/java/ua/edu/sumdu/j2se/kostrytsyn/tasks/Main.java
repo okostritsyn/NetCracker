a1d425fa10a0efb90697e23787c254236e0b30eb
Oleg Kostrytsyn <okostritsyn@gmail.com>
2022-05-10 01:48:58 +0300

    add class Task Made algorithm and check style

--- src/main/java/ua/edu/sumdu/j2se/kostrytsyn/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/kostrytsyn/tasks/Main.java
@@ -1,9 +1,67 @@
 package ua.edu.sumdu.j2se.kostrytsyn.tasks;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		//create new tasks
+		Task Task1 = new Task("test1",12,11,2); //error
+		Task Task2 = new Task("test2",12,15,1); //repeatable
+		Task Task3 = new Task("test3",16); //simple
+		Task Task4 = new Task("test3",12,15,4); //repeatable with interval bigger than end
 
-	}
+		//settings parameters
+		Task1.setActive(true);
+		Task2.setActive(true);
+		Task3.setActive(true);
+		Task4.setActive(true);
+
+		System.out.println(Task1.getTitle());
+		System.out.println(Task1.nextTimeAfter(11)); //12
+
+		System.out.println(Task2.getTitle());
+		System.out.println(Task2.nextTimeAfter(13)); //13
+
+		System.out.println(Task3.getTitle());
+		System.out.println(Task3.nextTimeAfter(14)); //16
+
+		System.out.println(Task4.getTitle());
+		System.out.println(Task4.nextTimeAfter(16)); // -1
+
+		//changing configurations
+		Task1.setTitle("let`s run!");
+		Task1.setTime(9);
+
+		Task2.setTitle("Keep running! Make a stop every hour");
+		Task2.setTime(10,18,1);
+
+		Task3.setTitle("You need to eat! Make a stop 2 hours");
+		Task3.setTime(19,22,2);
+
+		Task4.setTitle("it`s time to sleep");
+		Task4.setTime(23);
+
+		System.out.println("In what time you need to run?");
+		System.out.println(Task1.getStartTime());
+
+		System.out.println("When the next stop?");
+		System.out.println(Task2.nextTimeAfter(Task1.getStartTime())); //10
+
+		System.out.println("When i can eat after run?");
+		System.out.println(Task3.nextTimeAfter(Task2.getStartTime()));
+
+		System.out.println("And if i start eating after 20?");
+		System.out.println(Task3.nextTimeAfter(20));
+
+		System.out.println("In what time you need to finish eating?");
+		System.out.println(Task3.getEndTime());
+
+		Task4.setActive(false);
+
+		System.out.println("Can i sleep?");
+		System.out.println(Task4.isActive());
+
+		}
 }
---
+++ src/main/java/ua/edu/sumdu/j2se/kostrytsyn/tasks/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This module is for students
+  * @since 1.0
+ * @author NetCracker
+ * @version 1.1
+ */
+package ua.edu.sumdu.j2se.kostrytsyn.tasks;
