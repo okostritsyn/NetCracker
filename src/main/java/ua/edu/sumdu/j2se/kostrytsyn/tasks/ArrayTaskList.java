9e146ef36ec620899bc61a79ba2f07d6e4fc7a2f
Oleg Kostrytsyn <okostritsyn@gmail.com>
2022-05-12 09:24:28 +0300

    add class ArrayTaskList Made algorithm for array

---
+++ src/main/java/ua/edu/sumdu/j2se/kostrytsyn/tasks/ArrayTaskList.java
@@ -0,0 +1,65 @@
+package ua.edu.sumdu.j2se.kostrytsyn.tasks;
+
+import java.util.Arrays;
+
+public class ArrayTaskList {
+
+    private Task[] arrayTask;
+
+    public void add(Task task){
+        if (arrayTask == null) {
+            arrayTask = new Task[]{task};
+        } else {
+            int newCapacity = arrayTask.length + 1;
+            arrayTask = Arrays.copyOf(arrayTask, newCapacity);
+            arrayTask[arrayTask.length-1] = task;
+        }
+        System.out.println("Elements after add -- "+Arrays.toString(arrayTask));
+    }
+
+    public boolean remove(Task task){
+        if (arrayTask == null) {
+            return false;
+        } else {
+            int indexElementToBeDeleted  = -1;
+            int i = 0;
+            for (Task currentTask:
+                    arrayTask) {
+                if (currentTask.equals(task)) {
+                    indexElementToBeDeleted  = i;
+                }
+                i++;
+            }
+            if (indexElementToBeDeleted >= 0) {
+                arrayTask = removeElement(arrayTask,indexElementToBeDeleted);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int size(){
+        if (arrayTask == null) {
+            return -1;
+        } else {
+            return arrayTask.length;
+        }
+    }
+
+    public Task getTask(int index){
+        if (arrayTask == null) {
+            return null;
+        } else {
+            return arrayTask[index];
+        }
+    }
+
+    private Task[] removeElement( Task [] arr, int index ){
+        Task[] arrDestination = new Task[arr.length - 1];
+        int remainingElements = arr.length - ( index + 1 );
+        System.arraycopy(arr, 0, arrDestination, 0, index);
+        System.arraycopy(arr, index + 1, arrDestination, index, remainingElements);
+        System.out.println("Elements after remove -- "  + Arrays.toString(arrDestination));
+        return arrDestination;
+    }
+ }
--- src/main/java/ua/edu/sumdu/j2se/kostrytsyn/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/kostrytsyn/tasks/Main.java
@@ -7,11 +7,27 @@ public class Main {
 	public static void main(String[] args) {
 
 		//create new tasks
-		Task Task1 = new Task("test1",12,11,2); //error
-		Task Task2 = new Task("test2",12,15,1); //repeatable
-		Task Task3 = new Task("test3",16); //simple
-		Task Task4 = new Task("test3",12,15,4); //repeatable with interval bigger than end
+		Task task1 = new Task("test1",12,11,2); //error
+		Task task2 = new Task("test2",12,15,1); //repeatable
+		Task task3 = new Task("test3",16); //simple
+		Task task4 = new Task("test3",12,15,4); //repeatable with interval bigger than end
 
+		ArrayTaskList TaskArr = new ArrayTaskList();
+		System.out.println(TaskArr.size());
+		TaskArr.add(task1);
+		System.out.println(TaskArr.size());
+		TaskArr.add(task2);
+		System.out.println(TaskArr.size());
+		TaskArr.add(task3);
+		System.out.println(TaskArr.size());
+
+		boolean status = TaskArr.remove(task4);
+		System.out.println("Remove element status "+status+" new size:"+TaskArr.size());
+
+		Task currentTask = TaskArr.getTask(0);
+		System.out.println(currentTask.getTitle());
+
+		/*
 		//settings parameters
 		Task1.setActive(true);
 		Task2.setActive(true);
@@ -62,6 +78,6 @@ public class Main {
 
 		System.out.println("Can i sleep?");
 		System.out.println(Task4.isActive());
-
+		*/
 		}
 }
