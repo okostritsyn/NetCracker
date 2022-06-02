707477d80f64d3d457d3db4a37a69ca899cb0d9d
Oleg Kostrytsyn <okostritsyn@gmail.com>
2022-06-02 09:25:29 +0300

    made method calendar in Tasks change method incoming to pass tests

--- src/main/java/ua/edu/sumdu/j2se/kostrytsyn/tasks/Tasks.java
+++ src/main/java/ua/edu/sumdu/j2se/kostrytsyn/tasks/Tasks.java
@@ -1,8 +1,6 @@
 package ua.edu.sumdu.j2se.kostrytsyn.tasks;
 
 import java.time.LocalDateTime;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.stream.Collectors;
@@ -11,22 +9,17 @@ import java.util.stream.StreamSupport;
 
 public class Tasks {
     public SortedMap<LocalDateTime, Set<Task>> calendar(Iterable<Task> tasks, LocalDateTime start, LocalDateTime end){
+        Stream<Task> currStream = StreamSupport.stream(tasks.spliterator(), false);
+
         return null;
     }
 
     public static Iterable<Task> incoming(Iterable<Task> tasks, LocalDateTime start,
                                           LocalDateTime end) {
-
-        System.out.println("tasks = " + tasks + ", start = " + start + ", end = " + end);
         Stream<Task> currStream = StreamSupport.stream(tasks.spliterator(), false);
-        List<Task> currList = currStream.filter((t) -> (t.nextTimeAfter(start) != null &&
-                t.nextTimeAfter(start).isAfter(start))
-                || (t.nextTimeAfter(start) != null
-                    && t.nextTimeAfter(start).isEqual(start)
-                    && t.nextTimeAfter(start).isBefore(end)) ||
-                (t.nextTimeAfter(start) != null
-                        && t.nextTimeAfter(start).isEqual(end))).collect(Collectors.toList());
-        return currList;
+        return currStream.filter((t) -> ((t.nextTimeAfter(start) != null && t.nextTimeAfter(start).isAfter(start)
+                    || t.nextTimeAfter(start) != null && t.nextTimeAfter(start).isEqual(start))
+                    && (t.nextTimeAfter(start) != null && t.nextTimeAfter(start).isBefore(end))
+                        || t.nextTimeAfter(start) != null && t.nextTimeAfter(start).isEqual(end))).collect(Collectors.toList());
      }
-
 }
